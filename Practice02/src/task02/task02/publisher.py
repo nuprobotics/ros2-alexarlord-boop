891c3969868ce1a8d538761a29a983678a003c63
alexarlord-boop <rezerw0.1@yandex.ru>
2024-10-19 17:30:57 +0300

    ROS-2024 Task02: Updated publisher.py

--- Practice02/src/task02/task02/publisher.py
+++ Practice02/src/task02/task02/publisher.py
@@ -1,44 +1,29 @@
 import rclpy
-from rclpy.node import Node
 from std_msgs.msg import String
+from rclpy.node import Node
 
 
-class PublisherNode(Node):
+class Publisher(Node):
     def __init__(self):
-        super().__init__('publisher_node')
+        super(Publisher, self).__init__("publisher")
+        self.declare_parameter("topic_name", "")
+        self.declare_parameter("text", "")
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.topic_name = self.get_parameter("topic_name").get_parameter_value().string_value
+        self.publisher = self.create_publisher(String, self.topic_name, 10)
 
-        self.declare_parameter('text', 'Hello, ROS2!')
-        self.declare_parameter('topic_name', '/spgc/receiver')
-
-        self.text = self.get_parameter('text').get_parameter_value().string_value
-        self.topic_name = self.get_parameter('topic_name').get_parameter_value().string_value
-
-        self.publisher_ = self.create_publisher(String, self.topic_name, 10)
-
-        self.get_logger().info(f"Publishing on topic: {self.topic_name}")
-        self.get_logger().info(f"Message text: {self.text}")
-
-        self.timer = self.create_timer(1.0, self.publish_message)
-
-    def publish_message(self):
+    def timer_callback(self):
         msg = String()
-        msg.data = self.text
-        self.publisher_.publish(msg)
-        self.get_logger().info(msg.data)
+        msg.data = self.get_parameter("text").get_parameter_value().string_value
+        self.publisher.publish(msg)
 
 
-def main(args=None):
-    rclpy.init(args=args)
-
-    node = PublisherNode()
-
-    try:
-        rclpy.spin(node)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        node.destroy_node()
-        rclpy.shutdown()
+def main():
+    rclpy.init()
+    node = Publisher()
+    rclpy.spin(node)
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
