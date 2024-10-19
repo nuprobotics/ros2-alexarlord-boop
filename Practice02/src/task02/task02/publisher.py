839173badd178463ad5bd9b368d8ae9a98347f82
alexarlord-boop <rezerw0.1@yandex.ru>
2024-10-19 17:13:07 +0300

    ROS-2024 Task02: Updated publisher.py

--- Practice02/src/task02/task02/publisher.py
+++ Practice02/src/task02/task02/publisher.py
@@ -9,14 +9,14 @@ class Publisher(Node):
         self.declare_parameter('text', 'Hello, ROS2!')
         self.declare_parameter('topic_name', '/spgc/receiver')
 
-        text = self.get_parameter('text').get_parameter_value().string_value
-        topic_name = self.get_parameter('topic_name').get_parameter_value().string_value
+        self.text = self.get_parameter('text').get_parameter_value().string_value
+        self.topic_name = self.get_parameter('topic_name').get_parameter_value().string_value
 
-        self.publisher_ = self.create_publisher(String, topic_name, 10)
+        self.publisher_ = self.create_publisher(String, self.topic_name, 10)
         self.timer = self.create_timer(2.0, self.timer_callback)
 
-        self.msg = String()
-        self.msg.data = text
+        msg = String()
+        msg.data = self.text
 
     def timer_callback(self):
         self.publisher_.publish(self.msg)
