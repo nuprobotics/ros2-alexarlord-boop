1865088950b29cb959cad68c81d26bd138bd1fda
alexarlord-boop <rezerw0.1@yandex.ru>
2024-10-19 16:14:15 +0300

    ROS-2024 Task02: fixed config topic name, refactored publisher code

--- Practice02/src/task02/task02/publisher.py
+++ Practice02/src/task02/task02/publisher.py
@@ -1,7 +1,6 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-from rclpy.parameter import Parameter
 
 
 class Publisher(Node):
@@ -9,11 +8,13 @@ class Publisher(Node):
         super().__init__('publisher')
         self.declare_parameter('text', 'Hello, ROS2!')
         self.declare_parameter('topic_name', '/spgc/receiver')
+
         text = self.get_parameter('text').get_parameter_value().string_value
         topic_name = self.get_parameter('topic_name').get_parameter_value().string_value
+
         self.publisher_ = self.create_publisher(String, topic_name, 10)
-        timer_period = 2.0
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.timer = self.create_timer(2.0, self.timer_callback)
+
         self.msg = String()
         self.msg.data = text
 
