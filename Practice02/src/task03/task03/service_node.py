417664dd74acf0fc39381f00653554dc4853cdc0
alexarlord-boop <rezerw0.1@yandex.ru>
2024-10-22 18:28:58 +0300

    ROS-2024 Task03: Updated service_node.py according to slides

--- Practice02/src/task03/task03/service_node.py
+++ Practice02/src/task03/task03/service_node.py
@@ -5,32 +5,22 @@ from std_srvs.srv import Trigger
 
 class ServiceNode(Node):
     def __init__(self):
-        super().__init__('service_node')
+        super(ServiceNode, self).__init__('service_node')
 
-        self._declare_parameters()
-        self._initialize_service_client()
-        self._initialize_service()
-
-    def _declare_parameters(self):
         self.declare_parameter("service_name", "")
         self.declare_parameter("default_string", "")
-
         self.service_name = self.get_parameter("service_name").get_parameter_value().string_value
         self.default_string = self.get_parameter("default_string").get_parameter_value().string_value
 
-    def _initialize_service_client(self):
         self.client = self.create_client(Trigger, "/spgc/trigger")
         self.string_to_store = ""
-
+        self.service = self.create_service(Trigger, self.service_name, self.service_callback)
         if not self.client.wait_for_service(timeout_sec=1.0):
             self.string_to_store = self.default_string
         else:
             future = self.client.call_async(Trigger.Request())
             rclpy.spin_until_future_complete(self, future)
             self.string_to_store = future.result().message
-
-    def _initialize_service(self):
-        self.service = self.create_service(Trigger, self.service_name, self.service_callback)
 
     def service_callback(self, request, response):
         response.success = True
