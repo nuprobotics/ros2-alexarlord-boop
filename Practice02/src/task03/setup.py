175c1f136311a9629f86ce2389ed7f9b2db98945
alexarlord-boop <rezerw0.1@yandex.ru>
2024-10-23 20:50:09 +0300

    ROS-2024 Task03: Updated solution

--- Practice02/src/task03/setup.py
+++ Practice02/src/task03/setup.py
@@ -7,7 +7,8 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/config', ['config/task03.yaml']),
         ('share/' + package_name + "/launch", ['launch/task03.launch'])
@@ -21,7 +22,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "service_node = task03.service_node:main"
+            "service = task03.service:main"
         ],
     },
 )
