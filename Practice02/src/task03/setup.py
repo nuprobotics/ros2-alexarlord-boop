536be3948a4fd368f19302f3e6fb65864e08055a
alexarlord-boop <rezerw0.1@yandex.ru>
2024-10-23 20:36:11 +0300

    ROS-2024 Task03: Updated setup and config

--- Practice02/src/task03/setup.py
+++ Practice02/src/task03/setup.py
@@ -9,8 +9,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/task03.launch']),
         ('share/' + package_name + '/config', ['config/task03.yaml']),
+        ('share/' + package_name + "/launch", ['launch/task03.launch'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
